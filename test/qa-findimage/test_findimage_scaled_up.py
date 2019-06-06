3f01df3f2cb9b41ec61dbf572c4f227bb1b97264
Albert Casals <skarbat@gmail.com>
2019-06-06 08:09:31 +0000

    Adding tests for scaled up icons - currently failing as expected

---
+++ test/qa-findimage/test_findimage_scaled_up.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#
+# Python pytest module for the qa-findimage,
+# using icon images that are scaled up - they are bigger than on the source image.
+#
+
+import os
+import json
+
+
+def find_dashboard_icon(image_file):
+    '''
+    Runs the findimage tool, returns True if image_file is found
+    '''
+    qa_findimage='../tools/qa-findimage/qa-findimage'
+    source_image='qa-findimage/data/kano-dashboard.png'
+    data = os.popen('{} {} {}'.format(qa_findimage, source_image, image_file)).read()
+    json_data = json.loads(data)
+    return json_data['found'] == True
+    
+
+def test_find_minecraft():
+    assert(find_dashboard_icon('qa-findimage/data/140x140/kano-code.png') == True)
+
+def test_find_scratch():
+    assert(find_dashboard_icon('qa-findimage/data/140x140/make-minecraft.png') == True)
+
+def test_find_projects():
+    assert(find_dashboard_icon('qa-findimage/data/140x140/pong.png') == True)
